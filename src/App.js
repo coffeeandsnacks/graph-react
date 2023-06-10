0c8b604b7e25598da79e0b254b867431ee0ce0f3
Jan W <wiegner.jan@gmail.com>
2023-06-10 20:01:52 +0200

    Add Histograms and clean up code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // @ts-check
 import './App.css';
 import React, { useEffect, useState } from 'react'
+import Plotly from 'plotly.js-dist'
 
 const APIURL = "https://gateway.thegraph.com/api/d3dd46dedd6bd87177046327502db49a/subgraphs/id/D7azkFFPFT5H8i32ApXLr34UQyBfxDAfKoCEK4M832M6"
 
 const query = `
   query {
-    highestSwaps: swaps(first: 5, orderDirection: desc, orderBy: amountUSD) {
+    highestSwaps: swaps(first: 1000, orderDirection: desc, orderBy: amountUSD) {
       ...DataOnSwap
     }
-    lowestSwaps: swaps(first: 5, orderDirection: asc, orderBy: amountUSD) {
+    lowestSwaps: swaps(first: 1000, orderDirection: asc, orderBy: amountUSD) {
       ...DataOnSwap
     }
   }
@@ -22,50 +23,168 @@ const query = `
   }
 `
 
-
-
 function App() {
-  const [data, setData] = useState(
-    /** @type {import('./types.d.ts').Data} */
-    ({
-      highestSwaps: [],
-      lowestSwaps: []
-    }))
-  
+  const [data, setData] = useState({
+    highestSwaps: [],
+    lowestSwaps: []
+  });
+
   useEffect(() => {
-    fetchData()
-  }, [])
+    // Fetch data when the component mounts
+    fetchData();
+  }, []);
 
   async function fetchData() {
-    const response = await fetch(APIURL, {method: "POST", body:JSON.stringify({
-      query
-    })}).then(response => response.json())
-    
-   const res = (/** @type {import('./types.d.ts').Req} */ (response))
-
-   setData(res.data);
+    const response = await fetch(APIURL, { method: "POST", body: JSON.stringify({ query }) });
+    const result = await response.json();
+  
+    const highestSwaps = result.data.highestSwaps.map(swap => parseFloat(swap.amountUSD));
+    const lowestSwaps = result.data.lowestSwaps.map(swap => parseFloat(swap.amountUSD));
+  
+    setData({ highestSwaps, lowestSwaps });
   }
 
-  console.log({swaps: data})
+  useEffect(() => {
+    // Create the total histogram chart when data changes
+    if (data.highestSwaps.length === 0 && data.lowestSwaps.length === 0) {
+      return; // Skip if data is not available yet
+    }
+  
+    const trace1 = {
+      x: data.highestSwaps,
+      type: 'histogram',
+      name: 'Highest Swaps',
+      opacity: 0.7,
+      marker: {
+        color: 'green',
+      },
+    };
+  
+    const trace2 = {
+      x: data.lowestSwaps,
+      type: 'histogram',
+      name: 'Lowest Swaps',
+      opacity: 0.7,
+      marker: {
+        color: 'red',
+      },
+    };
+  
+    const layout = {
+      title: 'Histogram of Swap Amounts',
+      xaxis: {
+        title: 'Swap Amounts (USD)',
+      },
+      yaxis: {
+        title: 'Frequency',
+      },
+      barmode: 'overlay', // Overlay the histograms
+      bargap: 0.1, // Gap between bars
+      bargroupgap: 0.2, // Gap between histogram groups
+    };
+  
+    const config = { responsive: true };
+  
+    Plotly.newPlot('myDiv', [trace1, trace2], layout, config);
+  }, [data]);
+
+
+  useEffect(() => {
+    // Create the histogram chart for highest swaps
+    if (data.highestSwaps.length === 0) {
+      return; // Skip if data is not available yet
+    }
+  
+    const trace1 = {
+      x: data.highestSwaps,
+      type: 'histogram',
+      name: 'Highest Swaps',
+      opacity: 0.7,
+      marker: {
+        color: 'green',
+      },
+    };
+  
+    const layout = {
+      title: 'Histogram of Highest Swaps',
+      xaxis: {
+        title: 'Swap Amounts (USD)',
+      },
+      yaxis: {
+        title: 'Frequency',
+      },
+      barmode: 'overlay', // Overlay the histograms
+      bargap: 0.1, // Gap between bars
+      bargroupgap: 0.2, // Gap between histogram groups
+    };
+  
+    const config = { responsive: true };
+  
+    Plotly.newPlot('highestSwapsChart', [trace1], layout, config);
+  }, [data.highestSwaps]);
+  
+  useEffect(() => {
+    // Create the histogram chart for lowest swaps
+    if (data.lowestSwaps.length === 0) {
+      return; // Skip if data is not available yet
+    }
+  
+    const trace1 = {
+      x: data.lowestSwaps,
+      type: 'histogram',
+      name: 'Lowest Swaps',
+      opacity: 0.7,
+      marker: {
+        color: 'red',
+      },
+    };
+  
+    const layout = {
+      title: 'Histogram of Lowest Swaps',
+      xaxis: {
+        title: 'Swap Amounts (USD)',
+      },
+      yaxis: {
+        title: 'Frequency',
+      },
+      barmode: 'overlay', // Overlay the histograms
+      bargap: 0.1, // Gap between bars
+      bargroupgap: 0.2, // Gap between histogram groups
+    };
+  
+    const config = { responsive: true };
+  
+    Plotly.newPlot('lowestSwapsChart', [trace1], layout, config);
+  }, [data.lowestSwaps]);
+
+  console.log({ swaps: data });
 
   return (
     <div>
+      <style>
+        {`.swaps-list {
+          word-wrap: break-word;
+        }`}
+      </style>
+      <script src="https://cdn.plot.ly/plotly-2.24.1.min.js"></script>
+      <div id="myDiv"></div>
+
       <h1>Highest</h1>
-      <ul className='swaps-list'>
-      {
-        data.highestSwaps.map((swap) => {
-          return <SwapListItem {...swap} />
-        })
-      }
-      </ul>
+      <div className='swaps-list'>
+        <h2>Highest Swaps</h2>
+        <div id="highestSwapsChart"></div>
+        <h2>All Highest Swaps</h2>
+        <div>{JSON.stringify(data.highestSwaps)}</div>
+        <h2>Accounts of Highest Swaps</h2>
+      </div>
+
       <h1>Lowest</h1>
-      <ul className='swaps-list'>
-      {
-        data.lowestSwaps.map((swap) => {
-          return <SwapListItem {...swap} />
-        })
-      }
-      </ul>
+      <div className='swaps-list'>
+        <h2>Lowest Swaps</h2>
+        <div id="lowestSwapsChart"></div>
+        <h2>All Lowest Swaps</h2>
+        <div>{JSON.stringify(data.lowestSwaps)}</div>
+      </div>
     </div>
   );
 }
@@ -75,12 +194,11 @@ export default App;
 /**
  * @param props {import('./types.d.ts').Swap}
  */
-function SwapListItem({ amountUSD, sender, timestamp, to }) {
-  const date = new Date(Number(timestamp) * 1000)
-  
-  return (
-    <li>
-      {date.toLocaleString('de-DE')} | {sender} sent {amountUSD} to {to}
-    </li>
-  )
-}
+// function SwapListItem({ amountUSD, sender, timestamp, to }) {
+//   const date = new Date(Number(timestamp) * 1000);
+//   return (
+//     <li>
+//       {date.toLocaleString('de-DE')} | {sender} sent {amountUSD} to {to}
+//     </li>
+//   );
+// }
